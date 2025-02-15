b98061400206b427f1bc42c53a392ac91fde10a9
Robie Laguda <robie>
2025-02-15 18:25:09 +0800

    enchance file

--- client-side/src/App.vue
+++ client-side/src/App.vue
@@ -3,6 +3,13 @@
 </script>
 
 <template>
-
-  <RouterView />
+  <div class="background-color">
+    <RouterView />
+  </div>
 </template>
+
+<style scoped>
+.background-color {
+  background: linear-gradient(to bottom, #b3e0ff, white);
+}
+</style> 
