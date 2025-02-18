91034afaf4b5021ae26f74a0f9ba6b3fe4624d23
Robie Laguda <robie>
2025-02-18 10:15:59 +0800

    layout with sidebar

--- client-side/src/router/index.js
+++ client-side/src/router/index.js
@@ -9,14 +9,28 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+
     {
       path: '/dashboard',
       name: 'dashboard',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/DashboardView.vue'),
+      component: () => import('../layouts/AdminLayout.vue'),
+      children: [
+        {
+          path: "/overview",
+          name: 'overview',
+          component: () => import("../views/DashboardView.vue")
+        },
+        {
+          path: '/orders/area',
+          name: 'order',
+          component: () => import('../views/OrdersView.vue'),
+        },
+      ],
     },
+   
   ],
 })
 
