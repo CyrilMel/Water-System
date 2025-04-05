5e4a9131b88c2c217e628b2ed17718fc40637216
Jan Robie Laguda <robie192002@gmail.com>
2025-04-05 11:29:33 +0800

    For Production deployment

--- server-side/server.js
+++ server-side/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from 'cors';
+import path from "path";
 import { connectDB } from "./config/db.js";
 
 import productRoutes from "./routes/product-router.js";
@@ -11,12 +12,21 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000
 
+const __dirname = path.resolve();
 app.use(cors());
 
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRouter);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "/client-side/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client-side", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
