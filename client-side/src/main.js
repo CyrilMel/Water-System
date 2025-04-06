fc07b89bec5f005248830c12e8318ddeb4c56ba5
Jan Robie Laguda <robie192002@gmail.com>
2025-04-06 12:55:11 +0800

    install pinia

--- client-side/src/main.js
+++ client-side/src/main.js
@@ -3,13 +3,16 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './axios'
 import VueFeather from 'vue-feather';
 
+const pinia = createPinia()
 const app = createApp(App)
 
 app.use(router)
+app.use(pinia)
 app.component(VueFeather.name, VueFeather)
 app.mount('#app')
