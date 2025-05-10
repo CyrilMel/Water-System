d130d996f89ac61f39fa464db7252cc927572dd8
Jan Robie Laguda <robie192002@gmail.com>
2025-05-10 21:45:51 +0800

    input real data

--- client-side/src/views/DashboardView.vue
+++ client-side/src/views/DashboardView.vue
@@ -4,119 +4,123 @@
     <div class="row">
       <div class="col-md-3">
         <div class="card p-3">
-          <h6>BOTTLES SOLD</h6>
-            <h3>136,712</h3>
-            <small class="text-secondary"><vue-feather class="text-success" type="arrow-up-right" :size="15" ></vue-feather>
-            2.4% From Last Month</small>
+          <h6 class="fw-semibold">TOTAL INCOME - APRIL</h6>
+            <h3 class="text-primary fw-semibold">₱36,840</h3>
+            <small class="text-secondary fw-semibold"><vue-feather class="text-success" type="arrow-up-right" :size="15" ></vue-feather>
+            2.40% From Last Month</small>
         </div>
       </div>
       <div class="col-md-3">
         <div class="card p-3">
-          <h6>TOTAL CUSTOMERS</h6>
-          <h3>250,615</h3>
-          <small class="text-secondary"><vue-feather class="text-danger" type="arrow-down-right" :size="15" ><small></small></vue-feather> 3.6% From Last Month</small>
+          <h6 class="fw-semibold">TOTAL EXPENSES - APRIL</h6>
+          <h3 class="text-primary fw-semibold">₱7,300</h3>
+          <small class="text-secondary fw-semibold"><vue-feather class="text-success" type="arrow-up-right" :size="15" ><small></small></vue-feather> 3.60% From Last Month</small>
         </div>
       </div>
       <div class="col-md-3">
         <div class="card p-3">
-          <h6>TOTAL ORDERS</h6>
-          <h3>871,812</h3>
-          <small class="text-success">⬆ 1.5% From Last Month</small>
+          <h6 class="fw-semibold">TOTAL REFILLS - APRIL</h6>
+          <h4 class="text-primary fw-semibold">984</h4>
+          <small class="text-success fw-semibold">⬆ 1.75% From Last Month</small>
         </div>
       </div>
       <div class="col-md-3">
         <div class="card p-3">
-          <h6>GALLONS SOLD</h6>
-          <h3>1,982</h3>
-          <small class="text-danger">⬇ 5.6% From Last Month</small>
+          <h6 class="fw-semibold">TOTAL GALLONS SOLD</h6>
+          <h3 class="text-primary fw-semibold">240 </h3>
+          <small class="text-success fw-semibold">⬆ 16 Increased this Month</small>
         </div>
       </div>
     </div>
-    
     <div class="row mt-4">
-      <div class="col-lg-8">
+      <div class="col-lg-7">
         <div class="card p-3">
-          <h5>Total Revenue</h5>
-          <h2 class="text-primary">₱106,720</h2>
+          <h5 class="fw-bold">Total Income for Year 2025</h5>
+          <h2 class="text-primary fw-bold">₱135,360</h2>
           <!-- Placeholder for chart -->
           <canvas id="balanceChart"></canvas>
         </div>
       </div>
-      <div class="col-lg-4">
-        <div class="card p-3 mb-4">
-          <h6>Expenses</h6>
-            <ul class="list-group">
-              <li class="list-group-item d-flex justify-content-between">
-                Employee Salary <span>₱670</span>
-              </li>
-              <li class="list-group-item d-flex justify-content-between">
-                Marketing <span>₱140</span>
-              </li>
-              <li class="list-group-item d-flex justify-content-between">
-                Bottle Packaging <span>₱100</span>
-              </li>
-              <li class="list-group-item d-flex justify-content-between">
-                Delivery Service <span>₱50</span>
-              </li>
-              <li class="list-group-item d-flex justify-content-between">
-                Other <span>₱350</span>
-              </li>
-            </ul>
-        </div>
-        <div class="card p-3">
-           <h6>Utilities Expenses</h6>
-            <ul class="list-group">
-              <li class="list-group-item d-flex justify-content-between">
-                Water <span>₱900</span>
-              </li>
-              <li class="list-group-item d-flex justify-content-between">
-                Electricity <span>₱1,500</span>
-              </li>
-              <li class="list-group-item d-flex justify-content-between">
-                Internet <span>₱1,2000</span>
-              </li>
-              <li class="list-group-item d-flex justify-content-between">
-                Gasoline <span>₱600</span>
-              </li>
-            </ul>
-        </div>
-      </div>
-    </div>
-    
-    <div class="row mt-4">
-      <div class="col-lg-8">
-        <div class="card p-3">
-          <div class="d-flex justify-content-between">
-            <h5>Shipment Monitoring</h5>
-            <p>See all <vue-feather type="arrow-up-right" :size="15" ></vue-feather> </p>
+      <div class="col-lg-5">
+          <div class="card p-3 mb-4">
+            <div style="max-height: 200px; min-height: 100px; overflow-y: auto;">
+              <ul class="list-group">
+                  <li class="list-group-item d-flex text-muted justify-content-between">
+                    Customers (Last Month) <span>77</span>
+                  </li>
+                  <li class="list-group-item d-flex text-muted justify-content-between">
+                    New Customers <span>3</span>
+                  </li>
+                  <li class="list-group-item d-flex text-muted justify-content-between">
+                    Total Customers <span>80</span>
+                  </li>
+                  <li class="list-group-item d-flex text-muted justify-content-between">
+                    Refills per month <span>984</span>
+                  </li>
+                  <li class="list-group-item d-flex text-muted justify-content-between">
+                    Refills per week <span>246</span>
+                  </li>
+                  <li class="list-group-item d-flex text-muted justify-content-between">
+                    Refills per day <span>35</span>
+                  </li>
+                </ul>
+            </div>
           </div>
+        <div class="card p-3">
           <table class="table">
             <thead>
               <tr>
-                <th>Tracking Number</th>
-                <th>Item</th>
-                <th>Total</th>
-                <th>Amount</th>
-                <th>Status</th>
-                <th>Estimated Arrival</th>
+                <th>Product Description</th>
+                <th>Cost</th>
+                <th>Selling Price</th>
+                <th>Mark-Up</th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td>251287128</td>
-                <td>5-Gallon Water Jug</td>
-                <td>10</td>
-                <td>₱2,500</td>
-                <td><span class="badge bg-primary">Delivery</span></td>
-                <td>April 28, 2023</td>
+                <td>Round</td>
+                <td>₱100</td>
+                <td>₱150</td>
+                <td>₱50</td>
               </tr>
               <tr>
-                <td>168272812</td>
-                <td>500ml Bottled Water</td>
-                <td>7</td>
-                <td>₱1,400</td>
-                <td><span class="badge bg-primary">Delivery</span></td>
-                <td>April 27, 2023</td>
+                <td>w/ Faucet</td>
+                <td>₱100</td>
+                <td>₱150</td>
+                <td>₱50</td>
+              </tr>
+            </tbody>
+          </table> 
+        </div>
+      </div>
+    </div>
+    <!-- <div class="row mt-4">
+      <div class="col-lg-8">
+        <div class="card p-3">
+          <div class="d-flex justify-content-between">
+            <h5 class="text-primary fw-bold">COSTING</h5>
+          </div>
+          <table class="table">
+            <thead>
+              <tr>
+                <th>Product Description</th>
+                <th>Cost</th>
+                <th>Selling Price</th>
+                <th>Mark-Up</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Round Gallon</td>
+                <td>100</td>
+                <td>₱150</td>
+                <td>₱50</td>
+              </tr>
+              <tr>
+                <td>Gallon w/ Faucet</td>
+                <td>100</td>
+                <td>₱120</td>
+                <td>₱20</td>
               </tr>
             </tbody>
           </table>
@@ -151,7 +155,7 @@
           </table>
         </div>
       </div>
-    </div>
+    </div> -->
   </div>
 </div>
 </template>
@@ -165,10 +169,10 @@ export default {
     new Chart(ctx, {
       type: 'line',
       data: {
-        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+        labels: ['Jan', 'Feb', 'Mar', 'Apr'],
         datasets: [{
           label: 'Revenue (₱)',
-          data: [20000, 25000, 22000, 27000, 30000, 32000],
+          data: [30840, 32840, 34840, 36840],
           fill: true,
           backgroundColor: 'rgba(54, 162, 235, 0.2)',
           borderColor: 'rgba(54, 162, 235, 1)',
